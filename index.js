34dfb9365d364ffd1d02ccac16b66664e15c331e
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2023-01-08 14:51:35 -0800

    Added styling to the website

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const intro = document.getElementById('welcome')
+function typing_effect(element, speed) { 
+    let text = "Hello, I'm Lillian Ho! :-)";
+    
+    element.innerHTML = "";
 
+    var i = 0;
+
+    var timer = setInterval(function() { 
+        if (i < text.length) {
+            element.append(text.charAt(i));
+            i++;
+        } else { 
+            clearInterval(timer);
+        }
+    }, speed)
+}
+
+typing_effect(intro, 80);
